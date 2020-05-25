cb0a69bc6a7546365f57fe5ecbd9f0c5b7b70ab1
Jeff <jefferson.zapata.sierra@gmail.com>
2020-05-25 18:06:17 -0500

    improve test names

--- test-instances/src/test/scala/logz/instances/TestLoggerSpec.scala
+++ test-instances/src/test/scala/logz/instances/TestLoggerSpec.scala
@@ -7,22 +7,22 @@ import minitest.SimpleTestSuite
 
 object TestLoggerSpec extends SimpleTestSuite {
 
-  testAsync("should log correctly") {
+  testAsync("should collect the logs in a list") {
     val result = for {
-      testLogger <- Ref.of[IO, List[String]](List.empty[String])
-      implicit0(logger: Logger[IO]) = TestLogger[IO](testLogger)
+      ref <- Ref.of[IO, List[String]](List.empty[String])
+      implicit0(logger: Logger[IO]) = TestLogger[IO](ref)
       _ <- Logger.debug[IO]("debug")
       _ <- Logger.info[IO]("info")
       _ <- Logger.warn[IO]("warn")
       _ <- Logger.error[IO]("error")
       _ <- Logger.error[IO, Exception](new Exception("Exception"))("error")
-      _ <- testLogger.get.map { logs =>
+      _ <- ref.get.map { logs =>
         assertEquals(logs.size, 5)
-        assertEquals(logs(0), "Level: ERROR, msg: error, exception: Exception")
-        assertEquals(logs(1), "Level: ERROR, msg: error")
-        assertEquals(logs(2), "Level: WARN, msg: warn")
-        assertEquals(logs(3), "Level: INFO, msg: info")
         assertEquals(logs(4), "Level: DEBUG, msg: debug")
+        assertEquals(logs(3), "Level: INFO, msg: info")
+        assertEquals(logs(2), "Level: WARN, msg: warn")
+        assertEquals(logs(1), "Level: ERROR, msg: error")
+        assertEquals(logs(0), "Level: ERROR, msg: error, exception: Exception")
       }
     } yield ()
     result.unsafeToFuture()
